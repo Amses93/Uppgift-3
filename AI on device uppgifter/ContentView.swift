d461578dd4580edddcf032f24f7bcc60926662b8
Amel Curic <amel.curic@softhouse.se>
2023-10-01 23:08:22 +0200

    Initial Commit

---
+++ AI on device uppgifter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI on device uppgifter
+//
+//  Created by Amel Curic on 2023-10-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
