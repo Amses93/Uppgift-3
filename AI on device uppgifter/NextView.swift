dca93eb86a07228fe7372071a032bfac1219c036
Amel Curic <amel.curic@softhouse.se>
2023-10-15 19:23:59 +0200

    first commit

---
+++ AI on device uppgifter/NextView.swift
@@ -0,0 +1,18 @@
+//
+//  NextView.swift
+//  AI on device uppgifter
+//
+//  Created by Amel Curic on 2023-10-15.
+//
+
+import SwiftUI
+
+struct NextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NextView()
+}
